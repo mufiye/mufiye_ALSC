55df2b436cd84c353351af5216addab47a3ddbe7
mufiye <mufiye140520@gmail.com>
2022-04-16 21:27:05 +0800

    add a training shell

---
+++ new_run_train_gat_our_twitter.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# the shell is for chaosuan cloud and gat our model
+
+module load anaconda/2021.05
+source activate mufiye_NLP
+
+# R-GAT in twitter
+python train.py --gat_noReshape_our --dataset_name twitter --highway --per_gpu_train_batch_size 8 --dropout 0.6 --num_mlps 1 --final_hidden_size 400
