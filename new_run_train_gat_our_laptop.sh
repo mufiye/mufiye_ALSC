e23b0519a6b2a869bba1f34b2e99ae43939a275e
mufiye <mufiye140520@gmail.com>
2022-04-16 22:33:53 +0800

    update shell for train laptop

--- new_run_train_gat_our_laptop.sh
+++ new_run_train_gat_our_laptop.sh
@@ -5,4 +5,4 @@ module load anaconda/2021.05
 source activate mufiye_NLP
 
 # R-GAT in laptop
-python train.py --gat_our --dataset_name laptop --highway --per_gpu_train_batch_size 32 --dropout 0.7 --num_layers 3 --hidden_size 400 --final_hidden_size 400
+python train.py --gat_noReshape_our --dataset_name laptop --highway --per_gpu_train_batch_size 32 --dropout 0.7 --num_layers 3 --hidden_size 400 --final_hidden_size 400
