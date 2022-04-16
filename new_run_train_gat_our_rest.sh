77f5c9ae093b88ed669898d987b235c8ddbd03c8
mufiye <mufiye140520@gmail.com>
2022-04-16 22:19:15 +0800

    update shell for new model training

---
+++ new_run_train_gat_our_rest.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# the shell is for chaosuan cloud and gat our model
+
+module load anaconda/2021.05
+source activate mufiye_NLP
+
+# GAT-our model and Restaurant dataset
+python train.py --gat_noReshape_our --dataset_name rest --highway --dropout 0.8 --per_gpu_train_batch_size 16 --num_layers 2 --num_mlps 2 --hidden_size 300 --final_hidden_size 300
