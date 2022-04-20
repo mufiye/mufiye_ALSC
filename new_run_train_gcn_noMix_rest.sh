e4e68c7a834bda68b2890ccdb8f1b804dce94ff0
mufiye <mufiye140520@gmail.com>
2022-04-20 23:25:46 +0800

    update noMix shell batchSize rest 16 and twitter 8

--- new_run_train_gcn_noMix_rest.sh
+++ new_run_train_gcn_noMix_rest.sh
@@ -6,5 +6,5 @@ source activate mufiye_NLP
 
 # rest
 python train.py --num_train_epochs 100 --gat_noMix_our --dataset_name rest --highway \
-                --dropout 0.8 --per_gpu_train_batch_size 64 --num_mlps 2 \
+                --dropout 0.8 --per_gpu_train_batch_size 16 --num_mlps 2 \
                 --hidden_size 100 --final_hidden_size 100 
--- new_run_train_gcn_noMix_twitter.sh
+++ new_run_train_gcn_noMix_twitter.sh
@@ -6,5 +6,5 @@ source activate mufiye_NLP
 
 # twitter
 python train.py --num_train_epochs 100 --gat_noMix_our --dataset_name twitter --highway \
-                --per_gpu_train_batch_size 64 --dropout 0.6 --num_mlps 2 \
+                --per_gpu_train_batch_size 8 --dropout 0.6 --num_mlps 2 \
                 --final_hidden_size 100 --gcn_dropout 0.0 
