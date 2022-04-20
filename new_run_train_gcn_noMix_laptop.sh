78a5695663d46c85d162358c48918c56e66f5d3c
mufiye <mufiye140520@gmail.com>
2022-04-20 20:34:02 +0800

    no_mix_model shell --num_layers 2

--- new_run_train_gcn_noMix_laptop.sh
+++ new_run_train_gcn_noMix_laptop.sh
@@ -5,4 +5,4 @@ module load anaconda/2021.05
 source activate mufiye_NLP
 
 # laptop
-python train.py --num_train_epochs 100 --gat_noMix_our --dataset_name laptop --highway --per_gpu_train_batch_size 32 --dropout 0.7 --hidden_size 100 --final_hidden_size 100
+python train.py --num_train_epochs 100 --gat_noMix_our --dataset_name laptop --highway --per_gpu_train_batch_size 32 --dropout 0.7 --hidden_size 100 --final_hidden_size 100 --num_layers 2
