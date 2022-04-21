65bca5aa5c5e7dc550d336e850f6a519b64c0983
mufiye <mufiye140520@gmail.com>
2022-04-21 13:58:07 +0800

    noMix rest twitter mlpNum 3 and large hidden size

--- new_run_train_gcn_noMix_rest.sh
+++ new_run_train_gcn_noMix_rest.sh
@@ -6,7 +6,7 @@ source activate mufiye_NLP
 
 # rest
 python train.py --num_train_epochs 100 --gat_noMix_our --dataset_name rest --highway \
-                --dropout 0.8 --per_gpu_train_batch_size 32 --num_mlps 2 \
+                --dropout 0.8 --per_gpu_train_batch_size 32 \
                 --seed 2019 \
-                --hidden_size 200 --final_hidden_size 200
+                --hidden_size 200 --final_hidden_size 200 --num_mlps 3
                 
--- new_run_train_gcn_noMix_twitter.sh
+++ new_run_train_gcn_noMix_twitter.sh
@@ -7,6 +7,6 @@ source activate mufiye_NLP
 # twitter
 # --per_gpu_train_batch_size 8 f1会高些
 python train.py --num_train_epochs 100 --gat_noMix_our --dataset_name twitter --highway \
-                --per_gpu_train_batch_size 32 --dropout 0.6 --num_mlps 2 \
+                --per_gpu_train_batch_size 32 --dropout 0.6 \
                 --gcn_dropout 0.0  \
-                --hidden_size 200 --final_hidden_size 200 
+                --hidden_size 200 --final_hidden_size 200 --num_mlps 3
