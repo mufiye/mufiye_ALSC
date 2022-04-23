acb8b669fca5aa0c84fd6827e61ad7a3a5530403
mufiye <mufiye140520@gmail.com>
2022-04-23 14:12:55 +0800

    update new dep model train shell

--- new_dep_model_rest_train.sh
+++ new_dep_model_rest_train.sh
@@ -5,7 +5,7 @@ module load anaconda/2021.05
 source activate mufiye_NLP
 
 # rest
-python train.py --num_train_epochs 100 --gat_noMix_our --dataset_name rest --highway \
+python train.py --num_train_epochs 100 --gat_noReshape_our --dataset_name rest --highway \
                 --seed 2019 \
                 --per_gpu_train_batch_size 32 \
                 --hidden_size 100 --final_hidden_size 100 \
--- new_dep_model_twitter_train.sh
+++ new_dep_model_twitter_train.sh
@@ -5,7 +5,7 @@ module load anaconda/2021.05
 source activate mufiye_NLP
 
 # twitter train
-python train.py --num_train_epochs 100 --gat_noMix_our --dataset_name twitter --highway \
+python train.py --num_train_epochs 100 --gat_noReshape_our --dataset_name twitter --highway \
                 --per_gpu_train_batch_size 32 \
                 --hidden_size 100 --final_hidden_size 100 \
                 --num_mlps 2 \
