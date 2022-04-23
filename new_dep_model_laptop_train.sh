dbfad217cb6d71bcb210698da27468718a516163
mufiye <mufiye140520@gmail.com>
2022-04-23 14:01:06 +0800

    new train shell for new dep model

---
+++ new_dep_model_laptop_train.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# the shell is for chaosuan cloud and gat our model
+
+module load anaconda/2021.05
+source activate mufiye_NLP
+
+# laptop train
+python train.py --num_train_epochs 100 --gat_noMix_our --dataset_name laptop --highway \
+                --per_gpu_train_batch_size 32 \
+                --hidden_size 100 --final_hidden_size 100 \
+                --num_layers 2 --num_mlps 3 \
+                --dropout 0.7 \
+                --gcn_dropout 0.2
---
+++ new_dep_model_twitter_train.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# the shell is for chaosuan cloud and gat our model
+
+module load anaconda/2021.05
+source activate mufiye_NLP
+
+# twitter train
+python train.py --num_train_epochs 100 --gat_noMix_our --dataset_name twitter --highway \
+                --per_gpu_train_batch_size 32 \
+                --hidden_size 100 --final_hidden_size 100 \
+                --num_mlps 2 \
+                --dropout 0.5 \
+                --gcn_dropout 0.0
