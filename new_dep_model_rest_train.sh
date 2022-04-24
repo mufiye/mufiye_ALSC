50998d536933b329eac88a80367a1b4b61459ec9
mufiye <mufiye140520@gmail.com>
2022-04-24 20:26:22 +0800

    adjust gcn men dim

--- new_dep_model_rest_train.sh
+++ new_dep_model_rest_train.sh
@@ -10,77 +10,78 @@ python train.py --num_train_epochs 100 --gat_noReshape_our --dataset_name rest -
                 --per_gpu_train_batch_size 32 \
                 --hidden_size 100 --final_hidden_size 100 \
                 --num_mlps 2 \
+                --gcn_mem_dim 100 \
                 --dropout 0.8 \
                 --gcn_dropout 0.0
 
-python train.py --num_train_epochs 100 --gat_noReshape_our --dataset_name rest --highway \
-                --seed 2019 \
-                --per_gpu_train_batch_size 32 \
-                --hidden_size 100 --final_hidden_size 100 \
-                --num_mlps 2 \
-                --dropout 0.8 \
-                --gcn_dropout 0.1
+# python train.py --num_train_epochs 100 --gat_noReshape_our --dataset_name rest --highway \
+#                 --seed 2019 \
+#                 --per_gpu_train_batch_size 32 \
+#                 --hidden_size 100 --final_hidden_size 100 \
+#                 --num_mlps 2 \
+#                 --dropout 0.8 \
+#                 --gcn_dropout 0.1
 
-python train.py --num_train_epochs 100 --gat_noReshape_our --dataset_name rest --highway \
-                --seed 2019 \
-                --per_gpu_train_batch_size 32 \
-                --hidden_size 100 --final_hidden_size 100 \
-                --num_mlps 2 \
-                --dropout 0.8 \
-                --gcn_dropout 0.2
+# python train.py --num_train_epochs 100 --gat_noReshape_our --dataset_name rest --highway \
+#                 --seed 2019 \
+#                 --per_gpu_train_batch_size 32 \
+#                 --hidden_size 100 --final_hidden_size 100 \
+#                 --num_mlps 2 \
+#                 --dropout 0.8 \
+#                 --gcn_dropout 0.2
 
-python train.py --num_train_epochs 100 --gat_noReshape_our --dataset_name rest --highway \
-                --seed 2019 \
-                --per_gpu_train_batch_size 32 \
-                --hidden_size 100 --final_hidden_size 100 \
-                --num_mlps 2 \
-                --dropout 0.8 \
-                --gcn_dropout 0.3
+# python train.py --num_train_epochs 100 --gat_noReshape_our --dataset_name rest --highway \
+#                 --seed 2019 \
+#                 --per_gpu_train_batch_size 32 \
+#                 --hidden_size 100 --final_hidden_size 100 \
+#                 --num_mlps 2 \
+#                 --dropout 0.8 \
+#                 --gcn_dropout 0.3
 
-python train.py --num_train_epochs 100 --gat_noReshape_our --dataset_name rest --highway \
-                --seed 2019 \
-                --per_gpu_train_batch_size 32 \
-                --hidden_size 100 --final_hidden_size 100 \
-                --num_mlps 2 \
-                --dropout 0.8 \
-                --gcn_dropout 0.4
+# python train.py --num_train_epochs 100 --gat_noReshape_our --dataset_name rest --highway \
+#                 --seed 2019 \
+#                 --per_gpu_train_batch_size 32 \
+#                 --hidden_size 100 --final_hidden_size 100 \
+#                 --num_mlps 2 \
+#                 --dropout 0.8 \
+#                 --gcn_dropout 0.4
 
-python train.py --num_train_epochs 100 --gat_noReshape_our --dataset_name rest --highway \
-                --seed 2019 \
-                --per_gpu_train_batch_size 32 \
-                --hidden_size 100 --final_hidden_size 100 \
-                --num_mlps 2 \
-                --dropout 0.8 \
-                --gcn_dropout 0.5
+# python train.py --num_train_epochs 100 --gat_noReshape_our --dataset_name rest --highway \
+#                 --seed 2019 \
+#                 --per_gpu_train_batch_size 32 \
+#                 --hidden_size 100 --final_hidden_size 100 \
+#                 --num_mlps 2 \
+#                 --dropout 0.8 \
+#                 --gcn_dropout 0.5
 
-python train.py --num_train_epochs 100 --gat_noReshape_our --dataset_name rest --highway \
-                --seed 2019 \
-                --per_gpu_train_batch_size 32 \
-                --hidden_size 100 --final_hidden_size 100 \
-                --num_mlps 2 \
-                --dropout 0.8 \
-                --gcn_dropout 0.6
+# python train.py --num_train_epochs 100 --gat_noReshape_our --dataset_name rest --highway \
+#                 --seed 2019 \
+#                 --per_gpu_train_batch_size 32 \
+#                 --hidden_size 100 --final_hidden_size 100 \
+#                 --num_mlps 2 \
+#                 --dropout 0.8 \
+#                 --gcn_dropout 0.6
 
-python train.py --num_train_epochs 100 --gat_noReshape_our --dataset_name rest --highway \
-                --seed 2019 \
-                --per_gpu_train_batch_size 32 \
-                --hidden_size 100 --final_hidden_size 100 \
-                --num_mlps 2 \
-                --dropout 0.8 \
-                --gcn_dropout 0.7
+# python train.py --num_train_epochs 100 --gat_noReshape_our --dataset_name rest --highway \
+#                 --seed 2019 \
+#                 --per_gpu_train_batch_size 32 \
+#                 --hidden_size 100 --final_hidden_size 100 \
+#                 --num_mlps 2 \
+#                 --dropout 0.8 \
+#                 --gcn_dropout 0.7
 
-python train.py --num_train_epochs 100 --gat_noReshape_our --dataset_name rest --highway \
-                --seed 2019 \
-                --per_gpu_train_batch_size 32 \
-                --hidden_size 100 --final_hidden_size 100 \
-                --num_mlps 2 \
-                --dropout 0.8 \
-                --gcn_dropout 0.8
+# python train.py --num_train_epochs 100 --gat_noReshape_our --dataset_name rest --highway \
+#                 --seed 2019 \
+#                 --per_gpu_train_batch_size 32 \
+#                 --hidden_size 100 --final_hidden_size 100 \
+#                 --num_mlps 2 \
+#                 --dropout 0.8 \
+#                 --gcn_dropout 0.8
 
-python train.py --num_train_epochs 100 --gat_noReshape_our --dataset_name rest --highway \
-                --seed 2019 \
-                --per_gpu_train_batch_size 32 \
-                --hidden_size 100 --final_hidden_size 100 \
-                --num_mlps 2 \
-                --dropout 0.8 \
-                --gcn_dropout 0.9
+# python train.py --num_train_epochs 100 --gat_noReshape_our --dataset_name rest --highway \
+#                 --seed 2019 \
+#                 --per_gpu_train_batch_size 32 \
+#                 --hidden_size 100 --final_hidden_size 100 \
+#                 --num_mlps 2 \
+#                 --dropout 0.8 \
+#                 --gcn_dropout 0.9
